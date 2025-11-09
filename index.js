8b1e1df6c0fdff2d8900050ba67b5f8410097c04
Abenezer <abitysank@gmail.com>
2025-11-09 03:03:32 -0500

    changes to html basic file structure and on js side, basic local storage, and possible post render logic setup

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const createPostBtn = document.getElementById("create-post")
-const themeToggle = document.getElementById("theme-toggle")
+const createPostBtn = document.getElementById("create-post");
+const themeToggle = document.getElementById("theme-toggle");
 
+function getPosts() {
+    return JSON.parse(localStorage.getItem("posts")) || [];
+}
 
+function savePosts(posts) {
+    localStorage.setItem("posts", JSON.stringify(posts));
+}
 
+document.addEventListener("DOMContentLoaded", () => {
+    const posts = getPosts();
+    renderPosts(posts);
+});
