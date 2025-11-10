20fa071f03a1570d0fc032346f5af78ce31a5fe1
Abenezer <abitysank@gmail.com>
2025-11-09 19:15:20 -0500

    more html cache and render function, an eadge case and card creating done with bootstrap element being send back to html.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const createPostBtn = document.getElementById("create-post");
 const themeToggle = document.getElementById("theme-toggle");
 const blogPostContainer = document.getElementById("blog-post");
+const savePostBtn = document.getElementById("savePostBtn");
+const titleInput = document.getElementById("titleInput");
+const contentInput = document.getElementById("contentInput");
 
 const posts = [];
 let editIndex = [];
@@ -18,7 +20,8 @@ function renderPosts() {
 
     blogPostContainer.innerHTML = "";
 
-    if (posts.innerHTML = 0) {
+    // dynamic no blog message
+    if ((posts.innerHTML = 0)) {
         blogPostContainer.innerHTML = `
             <span class="text-center text-muted d-block">
                 There are no posts yet — please create one by pressing
@@ -26,4 +29,21 @@ function renderPosts() {
             </span>
         `;
     }
+
+    // blog card
+    posts.forEach((post, index) => {
+        const card = document.getElementById("div");
+        card.classList = "card mb-4 w-50 mx-auto";
+        card.innerHTML = `
+            <div class="card-body">
+                <div class="small text-muted">${post.date}</div>
+                <h2 class="card-title">${post.title}</h2>
+                <p class="card-text">${post.content}</p>
+                <button class="btn btn-outline-primary btn-sm me-2" data-index="${index}" id="btn-read-more">Read more</button>
+                <button class="btn btn-outline-secondary btn-sm me-2" data-index="${index}" id="btn-edit">Edit</button>
+                <button class="btn btn-outline-danger btn-sm" data-index="${index}" id="btn-delete">Delete</button>
+            </div>
+        `;
+        blogPostContainer.appendChild(card);
+    });
 }
