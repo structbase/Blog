185eaaa5a2b26074a46cbebae1f188d896020b08
Abenezer <abitysank@gmail.com>
2025-11-09 19:06:48 -0500

    render function for blog cards started

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const createPostBtn = document.getElementById("create-post");
 const themeToggle = document.getElementById("theme-toggle");
+const blogPostContainer = document.getElementById("blog-post");
 
 const posts = [];
+let editIndex = [];
 
 function getPosts() {
     return JSON.parse(localStorage.getItem("posts")) || [];
@@ -9,4 +11,19 @@ function getPosts() {
 
 function savePosts(posts) {
     localStorage.setItem("posts", JSON.stringify(posts));
+}
+
+function renderPosts() {
+    const posts = getPosts();
+
+    blogPostContainer.innerHTML = "";
+
+    if (posts.innerHTML = 0) {
+        blogPostContainer.innerHTML = `
+            <span class="text-center text-muted d-block">
+                There are no posts yet — please create one by pressing
+                <strong>Create Post</strong>.
+            </span>
+        `;
+    }
 }
